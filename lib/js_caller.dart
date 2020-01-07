98ce969d76bc20474436b808347791e8fdc8c3fa
agajo <agajo@users.noreply.github.com>
2020-01-08 00:45:46 +0900

    play sound by wheel. better.

--- lib/js_caller.dart
+++ lib/js_caller.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:js' as js;
 
 class JsCaller {
@@ -13,7 +14,19 @@ class JsCaller {
     js.context.callMethod('setNote', <double>[frequency]);
   }
 
+  bool _isPlaying = false;
+  Timer _timer;
   void playLong(double frequency) {
-    js.context.callMethod('playLong', <double>[frequency]);
+    if (_isPlaying) {
+      js.context.callMethod('setNote', <double>[frequency]);
+    } else {
+      js.context.callMethod('play', <double>[frequency]);
+      _isPlaying = true;
+    }
+    _timer?.cancel();
+    _timer = Timer(const Duration(seconds: 3), () {
+      js.context.callMethod('stop');
+      _isPlaying = false;
+    });
   }
 }
--- web/my_audio.js
+++ web/my_audio.js
@@ -12,15 +12,3 @@ function stop(){
 function setNote(frequency){
     synth.setNote(frequency);
 }
-
-function playLong(frequency){
-    if(isPlaying){
-        synth.setNote(frequency);
-    }else{
-        synth.triggerAttackRelease(frequency,5);
-        isPlaying = true;
-        setTimeout(()=>{
-            isPlaying = false;
-        },5000);
-    }
-}
